67061993d3ad156dcd000a074d1fb2b70ec06a60
MalenaHuy <malenahuuy@gmail.com>
2020-04-27 00:01:45 -0700

    commit

--- app.js
+++ app.js
@@ -1,3 +1,54 @@
-function fn1(){
-    var 
-}
+// // allows only numeric value
+// function inputNum(){
+//     var char = string.fromCharCode(evt.which);
+//     if(!(/[0-9]/.test(char))){
+//         evt.preventDefault();
+//     }
+// }
+
+// function is_number($var)
+// {
+//     if ($var == (string) (float) $var) {
+//         return (bool) is_numeric($var);
+//     }
+//     if ($var >= 0 && is_string($var) && !is_float($var)) {
+//         return (bool) ctype_digit($var);
+//     }
+//     return (bool) is_numeric($var);
+// }
+
+// const showMessage = () =>{
+//     alert("hello")
+// }
+// const document = document.getElementById("num1");
+// document.addEventListener("keyup", showMessage);
+
+function myFunction() {
+    var x, text;
+  
+    // Get the value of the input field with id="numb"
+    x = document.getElementById("num1").value;
+  
+    // If x is Not a Number or less than one or greater than 10
+    if (isNaN(x) || x < 1 || x > 10) {
+      text = "Input not valid";
+    } else {
+      text = "Input OK";
+    }
+    document.getElementById("demo").innerHTML = text;
+  }
+
+  function myFunction() {
+    var x, text;
+  
+    // Get the value of the input field with id="numb"
+    x = document.getElementById("num2").value;
+  
+    // If x is Not a Number or less than one or greater than 10
+    if (isNaN(x) || x < 1 || x > 10) {
+      text = "Input not valid";
+    } else {
+      text = "Input OK";
+    }
+    document.getElementById("demo").innerHTML = text;
+  }
