a1f7f735da19c8fc4d835194ebaad90120984640
MalenaHuy <malenahuuy@gmail.com>
2020-04-28 23:20:45 -0700

    commit

--- app.js
+++ app.js
@@ -1,32 +1,57 @@
 function myFunction() {
   var x, text;
 
-  // Get the value of the input field with id="numb1"
+  // Get the value of the input field with id="numb"
   x = document.getElementById("num1").value;
-
+  y = document.getElementById("num2").value;
   // If x is Not a Number or less than 2 or greater than 100
-  if (isNaN(x) || x < 2 || x > 100) {
-    text1 = "Invalid input, please try again.";
+  if ((isNaN(x) || x < 2 || x > 100) && (isNaN(y) || y < 2 || y > 100)){
+    text = "Invalid input, please try again.";
+    document.getElementById("demo").innerHTML = text;
+    document.getElementById("demo").style.color= "red";
+    document.getElementById("demo").style.marginLeft = "25px";
   } else {
-    // Get the value of the input field with id="numb2"
-    x = document.getElementById("num2").value;
-      if (isNaN(x) || x < 2 || x > 100) {
-        text1="Invalid input, please try again."
-      }else{
-        text2 = "There are ... prime number"
+
+        var pirmeNumberlist = sumPrimes(x,y);
+        console.log(pirmeNumberlist)
+        text = pirmeNumberlist.toString();
+        document.getElementById("demo").innerHTML = text;
+        document.getElementById("demo").style.color= "teal";
+        document.getElementById("demo").style.marginLeft = "25px";
       }
-      document.getElementById("demo").innerHTML = text2;
-      document.getElementById("demo").style.color= "teal";
-      document.getElementById("demo").style.marginLeft = "25px";
+  
+}
+
+function sumPrimes(num1, num2) {
+  let primeArr = [];
+  nextPrime:
+  for (let i = num1; i <= num2; i++) {
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) continue nextPrime;
+    }
+    primeArr.push(i);
   }
-  document.getElementById("demo").innerHTML = text1;
-  document.getElementById("demo").style.color= "red";
-  document.getElementById("demo").style.marginLeft = "25px";
+  return primeArr;
 }
+// function arr() {
+//   num1= document.getElementById("num1").value;
+//   num2= document.getElementById("num2").value;
+//   let primArr = [];
+//   nextPrime:
+//   for (let i=2; i <=num2; i++){
+//     for (let j = num1; j<i; j++){
+//       if (i % j == 0) continue nextPrime;
+//     }
+//     primeArr.push (i);
+//   }
+//   return primArr
+// }
+// console.log(primArr)
+// text2 = "There are ... prime number"
 
-function myFunction2() {
-
-}
+// document.getElementById("demo").innerHTML = text;
+// document.getElementById("demo").style.color= "teal";
+// document.getElementById("demo").style.marginLeft = "25px";
 
 
 
